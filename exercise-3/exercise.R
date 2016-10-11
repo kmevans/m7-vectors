d1b556eb93d4f385826561b5f7ba4d3d79c086dc
Kyle Evans <kmevans@uw.edu>
2016-10-11 13:44:44 -0700

    Yolo

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,19 +1,26 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
-
+marbles <- c("Red", "Orange", "Yellow", "Green", "Blue", "Purple");
 
 # Use the `sample` function to select a single marble
-
+sample(marbles, 1);
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
-
+MarbleGame <- function(guess) {
+  drawing <- sample(marbles, 1);
+  if (guess == drawing) {
+    return(paste("You guessed ", guess, ", which is correct!"));
+  } else {
+    return(paste("You guessed ", guess, " which is incorrect. The correct answer was ", drawing, "."));
+  }
+}
 
 # Play the marble game!
-
+print(MarbleGame("Red"));
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
 
